c5bd550d39a045af6bea1bd2517a0dde9f3b6073
Eagle-CN <lusaigg@gmail.com>
2025-02-05 17:14:17 +0800

    Update vercel-bulid.sh

--- vercel-bulid.sh
+++ vercel-bulid.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+# 安装Python和distutils
 apt-get update
-apt-get install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
+apt-get install -y python3-distutils
+
+# 安装canvas依赖
+apt-get install -y \
+    build-essential \
+    libcairo2-dev \
+    libpango1.0-dev \
+    libjpeg-dev \
+    libgif-dev \
+    librsvg2-dev
